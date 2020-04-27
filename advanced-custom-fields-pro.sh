32fec9780397b7182a5b287988bc436c2c76b9fe
Eric Iffland <hello@imagineiff.com>
2020-04-27 00:47:38 -0700

    redundant comments, fix printf

--- advanced-custom-fields-pro.sh
+++ advanced-custom-fields-pro.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# This script is for keeping a private repo of ACF up to date; automatically tagging the version and committing changes.
-# Such a repo could then be included in composer dependency management, and might make more sense than running cedaro/satispress, depending on your use case.
-# 1. replace <your-acf-pro-id> with your ACF ID
-# 2. replace <your-git-username> with your git username/account/url
-# 3. Do the initial creation of your repo, tag, commit and push manually before running this script.
-
 # grab variables from config file
 source scripts/advanced-custom-fields-pro.config
 
@@ -37,7 +31,7 @@ if `git status | grep -q "nothing to commit"`; then
   printf "Plugin updated to $VERSION\n"
   # Single quotes print literal (don't expand variables, and print the double quotes)
   # In order to expand the VERSION variable, the single quote for the line is unquoted, the variable expanded in double quotes, and then the single quote resumed after.
-  printf '"'"$gituser"'"/advanced-custom-fields-pro": "'"$VERSION"'",'
+  printf '%s\n' '"'"$gituser"'/advanced-custom-fields-pro": "'"$VERSION"'",'
 else
   # No changes
   cp ../scripts/advanced-custom-fields-pro-composer.json composer.json
